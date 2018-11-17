05161093d92e6f9ec30e881064c578ff9dde1591
unknown <yosefmel@post.bgu.ac.il>
2018-11-17 14:26:16 +0200

    one test has passed

--- test/code-analyzer.test.js
+++ test/code-analyzer.test.js
@@ -1,11 +1,18 @@
 import assert from 'assert';
-import {parseCode} from '../src/js/code-analyzer';
+import {codeParse, initTable, parseCode} from '../src/js/code-analyzer';
+
 describe('The javascript parser', () => {
-    it('is parsing an empty function correctly', () => {;
+    it('Check If Assignment is working', () => {
+        let table1=codeParse(parseCode('x=4;'), initTable());
+        let table2= initTable();
+        table2[0].push(1);
+        table2[1].push('assignment expression');
+        table2[2].push('x');
+        table2[3].push('');
+        table2[4].push(4);
         assert.equal(
-            '1',
-            '1'
+            JSON.stringify(table1),
+            JSON.stringify(table2)
         );
     });
-
 });
